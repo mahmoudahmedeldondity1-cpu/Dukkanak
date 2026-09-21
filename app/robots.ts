f1e9b2662397638e6b618f78614eecdaad3d2cd8
mahmoudahmedeldondity1-cpu <mahmoudahmedeldondity1@gmail.com>
2026-09-21 09:23:23 +0300

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,17 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/admin/",
+        "/account/",
+        "/api/",
+      ],
+    },
+
+    sitemap: "https://dukanak.vercel.app/sitemap.xml",
+  };
+}
